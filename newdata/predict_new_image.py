d4763cef8c4007a519f888c95814a663ef1ccd19
kwchen16 <52929558+kwchen16@users.noreply.github.com>
2019-07-25 18:21:50 -0400

    Update and rename resize_recolor_image.py to predict_new_image.py

---
+++ newdata/predict_new_image.py
@@ -0,0 +1,30 @@
+#pip install Pillow
+from PIL import Image
+from keras.models import model_from_json
+import numpy
+import os
+import cv2
+
+#resize the images
+myimage=Image.open("modified_blue_jeans.jpeg")
+image=myimage.resize((28,28),Image.ANTIALIAS)
+image=image.convert('LA')
+image.save("modified_blue_jeans.png")
+
+#structurize the image to predict
+image=cv2.imread("modified_blue_jeans.png",0).astype("float32").reshape(1,28,28)
+image/=255
+#print(image.shape,image.dtype,image)
+
+#load json and create model
+json_file = open('model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+#load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+#test with new images
+pred = loaded_model.predict(image)
+print(pred)
